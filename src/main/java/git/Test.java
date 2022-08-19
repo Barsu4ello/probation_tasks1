0e8d8ef666a2b7af4d97c9ba00fe315ea6045d14
Vladislav Gorbunov <wint-mil@mail.ru>
2022-08-19 13:33:20 +0300

    static method2 br 2

--- src/main/java/git/Test.java
+++ src/main/java/git/Test.java
@@ -1,14 +1,8 @@
 package git;
 
 public class Test {
-    int s;
-
     public static void main(String[] args) {
-        f();
     }
 
-    private static void f() {
-        System.out.println();
-    }
 
 }
